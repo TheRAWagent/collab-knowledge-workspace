bd0956205cfd3d41a2abc7aaaaae5aa4a9ad4d78
Dhairya Joshi <77039476+TheRAWagent@users.noreply.github.com>
2026-02-28 23:58:11 +0530

    feat: Implement ai-service with observability, Flyway database migration for vector extension, and deployment configurations.

--- ai-service/build.gradle.kts
+++ ai-service/build.gradle.kts
@@ -15,16 +15,26 @@ java {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.ai:spring-ai-advisors-vector-store")
 	implementation("org.springframework.ai:spring-ai-starter-model-openai")
 	implementation("org.springframework.ai:spring-ai-starter-vector-store-pgvector")
+	implementation("org.springframework.boot:spring-boot-starter-flyway")
+  implementation("org.springframework.boot:spring-boot-starter-opentelemetry")
+  implementation("org.flywaydb:flyway-database-postgresql")
 	runtimeOnly("org.postgresql:postgresql")
+  implementation("io.micrometer:micrometer-registry-prometheus")
+	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jdbc-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-  implementation(platform(libs.spring.ai.bom))
+	testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+  implementation("ch.qos.logback:logback-classic")
+  implementation(libs.logstash.logback.encoder)
+	implementation(libs.springdoc.openapi.webmvc.scalar)
+	implementation(platform(libs.spring.ai.bom))
 }
 
 tasks.withType<Test> {
